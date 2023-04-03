816d23af501eddc21e06cdd25e3dd1c9e6086115
Adam Frazee <frazeeat@miamioh.edu>
2023-04-03 09:08:56 -0400

    Refactor game logic into a module

--- src/logic.rs
+++ src/logic.rs
@@ -1,65 +1,70 @@
-use rand::Rng;
-struct Player{
-    dice: Vec<i32>,
-}
-struct Game {
-    players: Vec<Player>,
-    dice_pile: Vec<i32>,
-    dice_count: i32,
-    dice_value: i32,
-}
-
-fn game_factory(number_of_players: i32) -> Game {
-        
-    let mut players: Vec<Player> = Vec::new();
-    let mut rng = rand::thread_rng();
-    for _ in 0..number_of_players{
-        let dice: Vec<i32> = (0..5).map(|_| rng.gen_range(1..=6)).collect();
-        let player: Player = Player { dice };
-        players.push(player);
+pub mod game{
+    use rand::Rng;
+    pub struct Player{
+        pub dice: Vec<i32>,
     }
-    let dice_pile: Vec<i32> =  determine_dice_pile(&players);
-    return Game{
-        players,
-        dice_pile,
-        dice_count: 1,
-        dice_value: 2
-    };
-}
-
-fn determine_dice_pile(players: &Vec<Player>) -> Vec<i32> {
-    let mut dice_pile: Vec<i32> = Vec::new();
-    for player in players{
-        dice_pile.extend(&player.dice);
+    pub struct Game {
+        pub players: Vec<Player>,
+        pub dice_pile: Vec<i32>,
+        pub dice_count: i32,
+        pub dice_value: i32,
     }
 
-    return dice_pile;
-}
+    pub fn game_factory(number_of_players: i32) -> Game {
 
-impl Game {
-    /// Used to determine win state of the players. Returns true if the call is correct.
-    fn call(&self) -> bool {
-        let count: usize = self.dice_pile.iter().filter(|&x| *x == self.dice_value || *x == 1).count();
-        return self.dice_count <= count as i32;
+        let mut players: Vec<Player> = Vec::new();
+        let mut rng = rand::thread_rng();
+        for _ in 0..number_of_players{
+            let dice: Vec<i32> = (0..5).map(|_| rng.gen_range(1..=6)).collect();
+            let player: Player = Player { dice };
+            players.push(player);
+        }
+        let dice_pile: Vec<i32> =  determine_dice_pile(&players);
+        return Game{
+            players,
+            dice_pile,
+            dice_count: 1,
+            dice_value: 2
+        };
     }
 
-    /// Used when a player would like to raise the current game state. Will return true and change game state if the raise is valid. 
-    /// False otherwise.
-    fn raise(&mut self, new_dice_count: i32, new_dice_value: i32) -> bool {
-        if new_dice_count < self.dice_count || ((new_dice_value <= self.dice_value) && (new_dice_count <= self.dice_count)) {
-            return  false;
-        } else {
-            self.dice_count = new_dice_count;
-            self.dice_value = new_dice_value;
-            return true;
+    pub fn determine_dice_pile(players: &Vec<Player>) -> Vec<i32> {
+        let mut dice_pile: Vec<i32> = Vec::new();
+        for player in players{
+            dice_pile.extend(&player.dice);
         }
 
+        return dice_pile;
+    }
+
+    impl Game {
+        /// Used to determine win state of the players. Returns true if the call is correct.
+        pub fn call(&self) -> bool {
+            let count: usize = self.dice_pile.iter().filter(|&x| *x == self.dice_value || *x == 1).count();
+            return self.dice_count <= count as i32;
+        }
+
+        /// Used when a player would like to raise the current game state. Will return true and change game state if the raise is valid. 
+        /// False otherwise.
+        pub fn raise(&mut self, new_dice_count: i32, new_dice_value: i32) -> bool {
+            if new_dice_count < self.dice_count || ((new_dice_value <= self.dice_value) && (new_dice_count <= self.dice_count)) {
+                return  false;
+            } else {
+                self.dice_count = new_dice_count;
+                self.dice_value = new_dice_value;
+                return true;
+            }
+
+        }
     }
 }
 
 #[cfg(test)]
 mod tests{
-    use super::*;
+    use super::game::Player as Player;
+    use super::game::Game as Game;
+    use super::game::*;
+
 
     fn mock_game() -> Game {
         let p1: Player = Player {
@@ -72,7 +77,7 @@ mod tests{
             dice: vec![2,5,2,2,1],
         };
         let dp : Vec<i32> = vec![1,2,4,2,6,3,1,6,2,5,2,2,1];
-        let mut g1: Game = Game {
+        let g1: Game = Game {
             players: vec![p1,p2,p3],
             dice_pile: dp,
             dice_count: 6,
@@ -83,13 +88,13 @@ mod tests{
 
     #[test]
     fn call_liars(){
-        let mut game = mock_game();
+        let game = mock_game();
         assert_eq!(game.call(), false);
     }
     #[test]
     fn make_game(){
         let num_play: i32 = 3;
-        let mut game : Game = game_factory(num_play);
+        let game : Game = game_factory(num_play);
         assert_eq!(game.players.len(), num_play as usize);
         assert_eq!(game.players[0].dice.len(), 5);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod logic;
 
 fn main() {
+    let g: logic::game::Game = logic::game::game_factory(3);
     println!("Hello, world!");
 }
